3fc86da69b2e66f5177f252c9d2878bdd566714a
John Kitzmiller <kitzy@kitzy.org>
2015-02-19 10:23:30 -0600

    Added flushDNS
    New Script - Flush DNS, support for 10.4 - 10.10

---
+++ flushDNS/flushDNS.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MAJOR_MAC_VERSION=$(sw_vers -productVersion | awk -F '.' '{print $1 "." $2}')
+
+if [ "${MAJOR_MAC_VERSION}" = "10.10" ]
+	then
+	    echo "Flushing DNS cache for Yosemite"
+		discoveryutil mdnsflushcache
+elif [ "${MAJOR_MAC_VERSION}" = "10.9" ] || [ "${MAJOR_MAC_VERSION}" = "10.8" ] || [ "${MAJOR_MAC_VERSION}" = "10.7" ]
+	then
+	    echo "Flushing DNS cache for Mavericks, Mountain Lion or Lion"
+		killall -HUP mDNSResponder
+elif [ "${MAJOR_MAC_VERSION}" = "10.6" ] || [ "${MAJOR_MAC_VERSION}" = "10.5" ] || [ "${MAJOR_MAC_VERSION}" = "10.4" ]
+	then
+	    echo "Flushing DNS cache for Snow Leopard and earlier"
+		dscacheutil -flushcache
+	else
+		echo "ERROR: OS not supported"
+		exit 1
+fi
+
+exit 0
