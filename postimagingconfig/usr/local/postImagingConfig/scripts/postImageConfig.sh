993c0341604f8ea318d52b79d5d01b3c045b03e7
John Kitzmiller <kitzy@kitzy.org>
2015-11-09 16:32:12 -0800

    Added support for OS X 10.11
    - Added support for 10.11
    - Auto detection of JAMF binary location
    - Replaced Big Honking Text with JAMF Helper

--- postimagingconfig/usr/local/postImagingConfig/scripts/postImageConfig.sh
+++ postimagingconfig/usr/local/postImagingConfig/scripts/postImageConfig.sh
@@ -1,62 +1,68 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Logging
 touch /var/log/postImagingConfig.log
-    
+
 # Redirect stderr to stdout and print all to log
 exec 2>&1 > >(tee -a /var/log/postImagingConfig.log)
 NOW="$(date +"%Y-%m-%d-%H-%M")"
 echo "Post Imaging Configuration started $NOW"
 
+icon="/usr/local/postImagingConfig/resources/icon.png"
+
+jamf=$(which jamf)
+
+jamfHelper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
+
 # Check if JAMF binary exists
-if [[ -e /usr/sbin/jamf ]]
+if [[ -e "$jamf" ]]
 
 	then /bin/echo "Jamf binary present, continuing as planned..."
-	
-	else /bin/echo "Jamf binary is not present, we need to halt" 
-	
+
+	else /bin/echo "Jamf binary is not present, we need to halt"
+
 	exit 1
 
 fi
-
-# Fires up Big Honking Text. Notifies the user that their machine is being modified. Mouse clicks are ignored.
-/usr/local/postImagingConfig/resources/BigHonkingText -w 100% -h 100% -m -p 0 "  Post imaging configuration in progress, please wait.  " &
 
 # Check to make sure the machine has enrolled before continuing
 /bin/echo "Checking enrollment..."
 until [ ! -f "/Library/Application Support/JAMF/FirstRun/Enroll/enroll.sh" ]
     do
         /bin/echo "Machine is not enrolled. Trying enrollment."
-        
+
         # Attempt enrollment
-        
+
         /Library/Application\ Support/JAMF/FirstRun/Enroll/enroll.sh
-        
+
         # Test if enrollment completed, sleep and try again if not
-        
+
         if [ ! -f "/Library/Application Support/JAMF/FirstRun/Enroll/enroll.sh" ]
         	then
         		break
-        	else 
+        	else
         		sleep 30
         fi
     done
 /bin/echo "Enrollment complete"
 
+# Fires up JAMF Helper. Notifies the user that their machine is being modified. Mouse clicks are ignored.
+"$jamfHelper" -windowType fs -icon $icon -description "Post imaging configuration in progress, please wait." &
+
 # Checking to make sure the JSS is available before proceeding
-/usr/sbin/jamf checkJSSConnection -retry 0
+"$jamf" checkJSSConnection -retry 0
 until [ $? = 0 ]
 	do
-		/usr/sbin/jamf checkJSSConnection -retry 0
+		"$jamf" checkJSSConnection -retry 0
 	done
 
 # Enforce management framework, update inventory and run all config policies
-/usr/sbin/jamf manage
-/usr/sbin/jamf recon
-/usr/sbin/jamf policy -event config
+"$jamf" manage
+"$jamf" recon
+"$jamf" policy -event config
 
 # Update inventory one last time since we installed software
-/usr/sbin/jamf recon
+"$jamf" recon
 
 # Clean up and reboot
 /bin/echo "Cleaning up..."
@@ -66,6 +72,6 @@ NOW="$(date +"%Y-%m-%d-%H-%M")"
 /bin/echo "Imaging completed $NOW"
 
 # Reboot
-/usr/sbin/jamf reboot -immediately
+"$jamf" reboot -immediately
 
 exit 0
