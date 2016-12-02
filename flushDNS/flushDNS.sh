de7eba80043ebb37f21f50d2fb439d2ffa173038
John Kitzmiller <kitzy@users.noreply.github.com>
2016-12-02 15:00:28 -0600

    Added support for 10.12

--- flushDNS/flushDNS.sh
+++ flushDNS/flushDNS.sh
@@ -2,9 +2,9 @@
 
 MAJOR_MAC_VERSION=$(sw_vers -productVersion | awk -F '.' '{print $1 "." $2}')
 
-if [ "${MAJOR_MAC_VERSION}" = "10.11" ] || [ "${MAJOR_MAC_VERSION}" = "10.10" ] || [ "${MAJOR_MAC_VERSION}" = "10.9" ] || [ "${MAJOR_MAC_VERSION}" = "10.8" ] || [ "${MAJOR_MAC_VERSION}" = "10.7" ]
+if [ "${MAJOR_MAC_VERSION}" = "10.12" ] || [ "${MAJOR_MAC_VERSION}" = "10.11" ] || [ "${MAJOR_MAC_VERSION}" = "10.10" ] || [ "${MAJOR_MAC_VERSION}" = "10.9" ] || [ "${MAJOR_MAC_VERSION}" = "10.8" ] || [ "${MAJOR_MAC_VERSION}" = "10.7" ]
 	then
-	    echo "Flushing DNS cache for Mavericks, Mountain Lion or Lion"
+	    echo "Flushing DNS cache for Sierra, El Capitan, Mavericks, Mountain Lion or Lion"
 		killall -HUP mDNSResponder
 elif [ "${MAJOR_MAC_VERSION}" = "10.6" ] || [ "${MAJOR_MAC_VERSION}" = "10.5" ] || [ "${MAJOR_MAC_VERSION}" = "10.4" ]
 	then
